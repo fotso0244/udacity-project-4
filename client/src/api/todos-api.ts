b6ffa40e378f8cc37fa41d7dbc3280276448ec1f
aristide <fotso0244@gmail.com>
2022-09-06 00:40:38 +0100

    Project 4

--- client/src/api/todos-api.ts
+++ client/src/api/todos-api.ts
@@ -8,10 +8,8 @@ export async function getTodos(idToken: string): Promise<Todo[]> {
   console.log('Fetching todos')
   console.log('token: ', idToken)
   
-  const response = await Axios.get(`/todos`, {
-    proxy: {
-      host: `${apiEndpoint}`
-    },
+  const response = await Axios.get(`${apiEndpoint}/todos`, {
+    
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${idToken}`
@@ -35,10 +33,8 @@ export async function createTodo(
   idToken: string,
   newTodo: CreateTodoRequest
 ): Promise<Todo> {
-  const response = await Axios.post(`/todos`,  JSON.stringify(newTodo), {
-    proxy: {
-      host: `${apiEndpoint}`
-    },
+  const response = await Axios.post(`${apiEndpoint}/todos`,  JSON.stringify(newTodo), {
+    
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${idToken}`
@@ -62,10 +58,8 @@ export async function patchTodo(
   todoId: string,
   updatedTodo: UpdateTodoRequest
 ): Promise<void> {
-  await Axios.patch(`/todos/${todoId}`, JSON.stringify(updatedTodo), {
-    proxy: {
-      host: `${apiEndpoint}`
-    },
+  await Axios.patch(`${apiEndpoint}/todos/${todoId}`, JSON.stringify(updatedTodo), {
+    
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${idToken}`
@@ -86,10 +80,8 @@ export async function deleteTodo(
   idToken: string,
   todoId: string
 ): Promise<void> {
-  await Axios.delete(`/todos/${todoId}`, {
-    proxy: {
-      host: `${apiEndpoint}`
-    },
+  await Axios.delete(`${apiEndpoint}/todos/${todoId}`, {
+    
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${idToken}`
@@ -108,10 +100,7 @@ export async function getUploadUrl(
   idToken: string,
   todoId: string
 ): Promise<string> {
-  const response = await Axios.post(`/todos/${todoId}/attachment`, '', {
-    proxy: {
-      host: `${apiEndpoint}`
-    },
+  const response = await Axios.post(`${apiEndpoint}/todos/${todoId}/attachment`, '', {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${idToken}`
